5486a6dcece63783e8e77958994d92c24345f44c
Marjan Magharehi <marjan.magharehi@gmail.com>
2019-11-14 12:25:24 +0000

    Added an if statement so that the welcome message is only implemented on the page if a name is added to the query string

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -16,7 +16,12 @@ get '/lionking' do
   "Oh, I just can't wait to be king!"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  @name = params[:name]
   erb(:index)
 end
