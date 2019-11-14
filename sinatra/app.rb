b4b14108023feae16dd719affb583c183bdd0d38
Marjan Magharehi <marjan.magharehi@gmail.com>
2019-11-14 09:43:37 +0000

    First web related commit

---
+++ sinatra/app.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+
+require 'shotgun'
+
+set :session_secret, 'super secret'
+
+get '/' do
+  "Hello, good morning!"
+end
+
+get '/secret' do
+  "That's why her hair is so big, it's full of secrets."
+end
+
+get '/lionking' do
+  "Oh, I just can't wait to be king!"
+end
+
+get '/cat' do
+  "<div style='border: dashed red'>
+    <img src='http://bit.ly/1eze8aE'>
+   </div>"
+end
