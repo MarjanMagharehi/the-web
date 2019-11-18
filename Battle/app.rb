a933ddea7b259a41a016d8810bf48afdd9cc546d
Marjan Magharehi <marjan.magharehi@gmail.com>
2019-11-18 16:17:01 +0000

    Added sessions, and removed the instance variables and stored the player names in the session

--- Battle/app.rb
+++ Battle/app.rb
@@ -4,15 +4,18 @@ class Battle < Sinatra::Base
   # get '/' do
   #   'Hello Battle!'
   # end
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
     erb(:play)
   end
+
 
   run! if app_file == $0
 end
