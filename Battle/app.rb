c445bab6f2f3228fc50421cdeb7a82965d4e97c5
Marjan Magharehi <marjan.magharehi@gmail.com>
2019-11-18 16:24:10 +0000

    Redirected /names to /play, to create another 'get' request from the /names route

--- Battle/app.rb
+++ Battle/app.rb
@@ -13,12 +13,13 @@ class Battle < Sinatra::Base
   post '/names' do
     session[:player_1_name] = params[:player_1_name]
     session[:player_2_name] = params[:player_2_name]
-    erb(:play)
+    redirect '/play'
   end
 
   get '/play' do
     @player_1_name = session[:player_1_name]
     @player_2_name = session[:player_2_name]
+    erb(:play)
   end
 
   run! if app_file == $0
