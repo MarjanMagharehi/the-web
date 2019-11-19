c0d706e207af14ceed2a60f3c27b3af84d2010b4
Marjan Magharehi <marjan.magharehi@gmail.com>
2019-11-19 15:49:52 +0000

    Write unit tests for the Player class

--- Battle/spec/player_spec.rb
+++ Battle/spec/player_spec.rb
@@ -1,5 +1,18 @@
 require 'player'
 
 describe Player do
-  it{is_expected.to be_instance_of(described_class)  }
+  subject(:bella) {Player.new("Bella")}
+
+  describe '#name' do
+    it 'returns the name attribute' do
+      expect(bella.name).to eq "Bella"
+    end
+  end
+
+  describe '#hp' do
+    it 'returns the default hp' do
+      expect(bella.hp).to eq(described_class::DEFAULT_HP)
+    end
+  end
+
 end
