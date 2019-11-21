6053c632456a53c3f332e1ea5b2f030e1376a2db
Marjan Magharehi <marjan.magharehi@gmail.com>
2019-11-21 11:03:38 +0000

    Wrote a feature test to test for displaying Player_1s HP points

--- Battle/spec/features/hit_points_spec.rb
+++ Battle/spec/features/hit_points_spec.rb
@@ -1,4 +1,8 @@
 feature 'Hit points' do
+  scenario 'show player 1s hit points' do
+    sign_in_and_play
+    expect(page).to have_content 'Bella: 100HP'
+  end
   scenario 'show player 2s hit points' do
     sign_in_and_play
     expect(page).to have_content 'Marjan: 100HP'
